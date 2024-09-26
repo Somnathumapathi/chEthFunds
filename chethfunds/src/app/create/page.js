338151dc8e426c75b00c26191918b606e65f2801
Koushik <koushik.mbp@gmail.com>
2024-09-27 02:29:37 +0530

    ui: create room page

--- chethfunds/src/app/create/page.js
+++ chethfunds/src/app/create/page.js
@@ -1,39 +1,60 @@
-'use client'
-import React from 'react'
-import { useAccount } from 'wagmi'
+// contractManager = msg.sender;
+//         memberSize = _memberSize;
+//         chitAmount = _chitAmount;
+//         // chitValue = _chitValue;
+//         chitValue = chitAmount * memberSize;
+//         remainingMonths = memberSize;
 
-const createRoom = () => {
-  // const { address } = useAccount()
+"use client";
+import React, { useEffect, useState } from 'react'
 
-  const startChit = async () => {
-    const memberSize = 5
-    const chitAmount = 0.01
+const create = () => {
+    const [memberSize, setMemberSize] = useState()
+    const [chitAmount, setChitAmount] = useState()
+    const [wallet, setWallet] = useState()
 
-    // try {
-    //   const res = await fetch('/api/startchit', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({
-    //         memberSize,
-    //         chitAmount,
-    //       })
-          
-    //   })
-    //   const data = await res.json()
-    //   console.log(data)
-    // } catch (error) {
-    //   console.error("Error creating room:", error);
-    // }
-  }
-  return (
-    <div>
-         <button onClick={startChit}>
-        Create ChitFund Room
-      </button>
-    </div>
-  )
+    const connectWallet = async () => {
+        const { ethereum } = window
+        if (!ethereum) return alert("Please install MetaMask!")
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+        const chainId = await ethereum.request({ method: 'eth_chainId' })
+        console.log(chainId)
+        const account = accounts[0]
+        setWallet(account)
+    }
+    useEffect(() => {
+        connectWallet()
+    }, [])
+
+    const createRoom = (e) => {
+        e.preventDefault();
+        if (!memberSize || !chitAmount) {
+            alert("Enter all data!")
+            return;
+        } if (!wallet) {
+            return alert("Connect wallet!!")
+        }
+        // TODO: Handle creation of new Chit Fund
+    }
+
+    return (
+        <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-background to-black'>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-semibold bg-clip-text bg-gradient-to-r from-primary to-accent text-transparent w-fit">CREATE CHIT FUND</h1>
+            </div>
+            {!wallet &&
+                <button className='bg-accent px-4 py-1 rounded-lg absolute top-20 right-20'>Connect Wallet</button>
+            }
+            <div className='flex justify-center items-center h-[400px] rounded-lg bg-primary/5 my-6'>
+                <form className='flex flex-col gap-6 m-10 w-max justify-center items-center'>
+                    <input type="text" value={memberSize} onChange={(e) => setMemberSize(e.target.value)} placeholder='Member Size' className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg w-fit' />
+                    <input type="text" value={chitAmount} onChange={(e) => setChitAmount(e.target.value)} placeholder='Chit Amount' className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg w-fit' />
+                    <input type="text" value={`${wallet}`} placeholder='Connect Wallet!!' disabled className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg cursor-not-allowed w-min' />
+                    <button type="submit" onClick={(e) => createRoom(e)} className='w-max px-4 py-1 border-2 border-accent rounded-lg bg-primary/10 mt-2'>Create</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
-export default createRoom
+export default create
--- chethfunds/src/app/join/page.js
+++ chethfunds/src/app/join/page.js
@@ -32,14 +32,16 @@ const join = () => {
 
     return (
         <div className='min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-background to-black'>
-            <h1 className="text-3xl font-semibold bg-clip-text bg-gradient-to-r from-primary to-accent text-transparent text-center">JOIN CHIT FUND</h1>
-            { !wallet &&
-            <button className='bg-accent px-4 py-1 rounded-lg absolute top-20 right-20'>Connect Wallet</button>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-semibold bg-clip-text bg-gradient-to-r from-primary to-accent text-transparent text-center">JOIN CHIT FUND</h1>
+            </div>
+            {!wallet &&
+                <button className='bg-accent px-4 py-1 rounded-lg absolute top-20 right-20'>Connect Wallet</button>
             }
             <div className='flex justify-center items-center h-[400px] rounded-lg bg-primary/5 my-6'>
                 <form className='flex flex-col gap-5 m-10 w-max justify-center items-center'>
                     <input type="text" value={roomId} onChange={(e) => setRoomId(e.target.value)} placeholder='Room ID' className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg w-fit' />
-                    <input type="text" value={wallet} onChange={(e) => setRoomId(e.target.value)} placeholder='Connect Wallet!!' disabled className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg cursor-not-allowed w-min' />
+                    <input type="text" value={wallet} placeholder='Connect Wallet!!' disabled className='bg-inherit px-4 py-1 active:border-0 border-b-2 border-accent rounded-lg cursor-not-allowed w-min' />
                     <button type="submit" onClick={(e) => joinRoom(e)} className='w-max px-4 py-1 border-2 border-accent rounded-lg bg-primary/10 mt-2'>Join</button>
                 </form>
             </div>
