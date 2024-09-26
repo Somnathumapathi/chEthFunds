080cb2418fefa3fa29811f5a8924b5600c24ff25
Somnathumapathi <somnathumapathi7@gmail.com>
2024-09-26 20:01:20 +0530

    contract integration

--- chethfunds/scripts/deploy.js
+++ chethfunds/scripts/deploy.js
@@ -1,9 +1,11 @@
 const { ethers } = require('hardhat');
 
+//0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+
 async function main() {
     // Specify the parameters for deployment
     const memberSize = 3; // Change as needed
-    const chitAmount = ethers.utils.parseUnits('1', 'ether'); // 1 ether
+    const chitAmount = ethers.parseUnits('1', 'ether'); // 1 ether
 
     // Get the contract factory
     const ChethFundFactory = await ethers.getContractFactory('ChethFund');
@@ -12,9 +14,9 @@ async function main() {
     const chethFund = await ChethFundFactory.deploy(memberSize, chitAmount);
 
     // Wait for the deployment to complete
-    await chethFund.deployed();
+    // await chethFund.deployed();
 
-    console.log(`ChethFund deployed to: ${chethFund.address}`);
+    console.log(`ChethFund deployed to: ${chethFund.target}`);
 }
 
 // Error handling
