f9e3088bc1ef43f9985a4c8b81e9809cdfdf60dd
Somnathumapathi <somnathumapathi7@gmail.com>
2024-09-26 23:57:51 +0530

    temp

--- chethfunds/scripts/deploy.js
+++ chethfunds/scripts/deploy.js
@@ -1,12 +1,19 @@
 const { ethers } = require('hardhat');
+const { JsonRpcProvider } = require("ethers");
 
 //0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+const provider = new JsonRpcProvider(process.env.RPC_URL);
 
 async function main() {
     // Specify the parameters for deployment
+    // cons(process.env.RPC_URL)
     const memberSize = 3; // Change as needed
     const chitAmount = ethers.parseUnits('1', 'ether'); // 1 ether
 
+    const chainId = await provider.send('eth_chainId', []);
+    console.log(`Chain ID: ${chainId}`);
+//     const networkName = chainIdToNetworkName[parseInt(chainId, 16)]; // Convert chainId to integer
+  console.log(`Network Name: ${(await provider.getNetwork()).name}`);
     // Get the contract factory
     const ChethFundFactory = await ethers.getContractFactory('ChethFund');
 
